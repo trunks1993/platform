d4c90a9c986694f3dc06ff2201e0ead726ad6e12
wangzhijian <604723443@qq.com>
2019-09-19 22:19:09 +0800

    fix: user search bug

--- src/views/system/user.vue
+++ src/views/system/user.vue
@@ -5,7 +5,7 @@
       :resetBtnVisible="false"
       :searchBtnVisible="true"
       :model="fqForm"
-      @afterFilter="handleFilter($event, getSysUserList)"
+      @afterFilter="handleFilter($event, query)"
     ></FilterQueryForm>
 
     <div class="app-wrapper" style="display: flex;">
